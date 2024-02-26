55727c78041836e2dea628b8fa505ae15ffd5098
andywang425 <13120989656@163.com>
2024-02-26 17:15:54 +0800

    🐳 chore: add `@` before each metadata label

--- src/providers/Completion/GMmetaCompletionProvider.ts
+++ src/providers/Completion/GMmetaCompletionProvider.ts
@@ -3,35 +3,44 @@ import GMmetaCompletionItems from './items/GMmetaCompletionItems';
 import checkIfShouldRun from '../../other/checkIfShouldRun';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-const GMmetaCompletionProvider = vscode.languages.registerCompletionItemProvider('javascript', {
-    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-        if (!checkIfShouldRun(document)) {
-            return;
+const GMmetaCompletionProvider = vscode.languages.registerCompletionItemProvider(
+  'javascript',
+  {
+    provideCompletionItems(
+      document: vscode.TextDocument,
+      position: vscode.Position,
+      token: vscode.CancellationToken,
+      context: vscode.CompletionContext
+    ) {
+      if (!checkIfShouldRun(document)) {
+        return;
+      }
+      const list: vscode.CompletionItem[] = GMmetaCompletionItems.map((item) => {
+        const completionItem = new vscode.CompletionItem('@' + item.label, item.kind);
+        completionItem.detail = 'GM Metadata';
+        completionItem.insertText = item.insertText ?? item.label + ' ';
+        const doc = new vscode.MarkdownString();
+        for (const i of item.documentation) {
+          switch (i.add) {
+            case 'markdown':
+              doc.appendMarkdown(i.value);
+              break;
+            case 'code':
+              doc.appendCodeblock(i.value, 'typescript');
+              break;
+            default:
+              doc.appendText(i.value);
+              break;
+          }
         }
-        const list: vscode.CompletionItem[] = GMmetaCompletionItems.map(item => {
-            const completionItem = new vscode.CompletionItem(item.label, item.kind);
-            completionItem.detail = 'GM Metadata';
-            completionItem.insertText = item.insertText ?? item.label + ' ';
-            const doc = new vscode.MarkdownString();
-            for (const i of item.documentation) {
-                switch (i.add) {
-                    case 'markdown':
-                        doc.appendMarkdown(i.value);
-                        break;
-                    case 'code':
-                        doc.appendCodeblock(i.value, 'typescript');
-                        break;
-                    default:
-                        doc.appendText(i.value);
-                        break;
-                }
-            }
-            completionItem.documentation = doc;
-            return completionItem;
-        });
+        completionItem.documentation = doc;
+        return completionItem;
+      });
 
-        return list;
+      return list;
     }
-}, '@');
+  },
+  '@'
+);
 
 export default GMmetaCompletionProvider;
